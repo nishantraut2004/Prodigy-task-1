db25c7be688ea714a0eefa627a5909a27f708f18
nishantraut2004 <nishantraut225@gmail.com>
2025-04-05 23:14:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change background color on scroll
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
